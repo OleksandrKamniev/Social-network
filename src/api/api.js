1ffacfa4a1754faf215747f538d576efe664b81d
OleksandrKamniev <sashkam75@gmail.com>
2022-12-09 14:22:48 +0200

    DAL, axios.create

---
+++ src/api/api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "d89c3d93-505c-4fd1-8ecf-c3bb9108133a"
+    }
+});
+
+
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data;
+            });
+    }
+}
