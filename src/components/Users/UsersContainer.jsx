733b0d269b2f482d29dd2bef12664d8e4ad03836
OleksandrKamniev <sashkam75@gmail.com>
2022-12-13 17:37:12 +0200

    disabled follow/unfollow
    If your interet connection is slow, you will not have an opportunity to activate the button more than 1 time untill it won't change

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -6,9 +6,9 @@ import {
     setUsers,
     setTotalUsersCount,
     toggleIsLoading,
-    unfollow
+    unfollow,
+    toggleFollowingProgress
 } from '../../redux/users-reducer';
-import axios from 'axios';
 import Users from './Users';
 import Preloader from "../../common/Preloader/Preloader";
 import { usersAPI } from '../../api/api';
@@ -32,7 +32,6 @@ class UsersContainer extends React.Component {
             this.props.setUsers(data.items);
         });
     }
-
     render() {
         return <>
             {this.props.isLoading ? <Preloader /> : null}
@@ -43,6 +42,8 @@ class UsersContainer extends React.Component {
                 users={this.props.users}
                 follow={this.props.follow}
                 unfollow={this.props.unfollow}
+                toggleFollowingProgress={this.props.toggleFollowingProgress}
+                followingInProgress={this.props.followingInProgress}
             />
         </>
     }
@@ -54,8 +55,10 @@ let mapStateToProps = (state) => {
         pageSize: state.usersPage.pageSize,
         totalUsersCount: state.usersPage.totalUsersCount,
         currentPage: state.usersPage.currentPage,
-        isLoading: state.usersPage.isLoading
+        isLoading: state.usersPage.isLoading,
+        isFetching: state.usersPage.isFetching,
+        followingInProgress: state.usersPage.followingInProgress
     }
 }
 export default connect(mapStateToProps,
-    { follow, unfollow, setUsers, setCurrentPage, setTotalUsersCount, toggleIsLoading })(UsersContainer);
+    { follow, unfollow, setUsers, setCurrentPage, setTotalUsersCount, toggleIsLoading, toggleFollowingProgress })(UsersContainer);
